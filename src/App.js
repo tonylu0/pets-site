c6d56df233b9fd5d395f7a9c7e1143bfa3a77c1d
lutny <lutny@amazon.com>
2024-01-24 23:19:37 -0600

    Removed extra text

--- src/App.js
+++ src/App.js
@@ -40,19 +40,8 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
         <MyButton onUpdate={updatePets} />
         <PetTable pets={pets} /> {/* Include the PetTable component */}
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
